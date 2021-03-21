c89b73c3aac31ca1fd152bc84f5594e3f7e94935
mayakrey <maya.krey@scout24.com>
2021-03-21 14:17:46 +0100

    Create random color on mouse pressed and change default to white

--- sketch.js
+++ sketch.js
@@ -7,10 +7,14 @@ function setup() {
 
 function draw() {
     if (mouseIsPressed) {
-        stroke(255, 255, 255);
+        stroke(createRandomNumber(0, 255), createRandomNumber(0, 255), createRandomNumber(0, 255));
     } else {
-        stroke(50, 200, 40);
+        stroke(255, 255, 255);
     }
     line(mouseX-30, mouseY-30, mouseX+30, mouseY+30);
     line(mouseX+30, mouseY-30, mouseX-30, mouseY+30);
+}
+
+function createRandomNumber(min, max) {
+    return Math.random() * (max - min) + min;
 }
